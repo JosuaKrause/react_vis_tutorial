0355dfb0ab3f157a017413279b4121d9151f8e8a
Josua Krause <josua.krause@gmail.com>
2017-09-15 14:38:56 -0400

    load data

--- src/BarChart.js
+++ src/BarChart.js
@@ -12,13 +12,47 @@ const Rect = styled.rect`
   fill: none;
 `
 
-class BarChart extends PureComponent {
+class Bars extends PureComponent {
   render() {
-    const { theme } = this.props;
-    const { width, height } = theme;
+    const { values, w, h } = this.props;
+    return (
+      <text x={w * 0.5} y={h * 0.5}>{values.length}</text>
+    );
+  }
+} // Bars
+
+class BarChart extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: [],
+    };
+    this.unmounted = false;
+  }
+
+  componentDidMount() {
+    fetch(`${window.PUBLIC_URL}/data.json`).then((data) => {
+      // TODO do error checking etc.
+      return data.json();
+    }).then((data) => {
+      if(this.unmounted) return;
+      this.setState({
+        values: data["values"],
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unmounted = true;
+  }
+
+  render() {
+    const { theme: { width, height } } = this.props;
+    const { values } = this.state;
     return (
       <Svg width={width} height={height}>
         <Rect x={0} y={0} width={width} height={height} />
+        <Bars values={values} w={width} h={height} />
       </Svg>
     );
   }
